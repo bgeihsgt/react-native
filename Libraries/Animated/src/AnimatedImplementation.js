3beda20d57942b49262ee7e0eb030dc798866674
Connor McEwen <connor.mcewen@gmail.com>
2016-08-15 09:28:43 -0400

    Export Animated.Interpolation

--- Libraries/Animated/src/AnimatedImplementation.js
+++ Libraries/Animated/src/AnimatedImplementation.js
@@ -2022,6 +2022,12 @@ module.exports = {
   ValueXY: AnimatedValueXY,
 
   /**
+   * Provides access to the AnimatedInterpolation type returned by calling
+   * interpolate on a value.
+   */
+  Interpolation: AnimatedInterpolation,
+
+  /**
    * Animates a value from an initial velocity to zero based on a decay
    * coefficient.
    */
